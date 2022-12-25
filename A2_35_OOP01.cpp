2b6a94c8edc1c00e32ce7eba10e90f02d7f82915
eLLuMInATi77 <117962781+eLLuMInATi77@users.noreply.github.com>
2022-12-25 21:45:40 +0530

    Update A2_35_OOP01.cpp

--- A2_35_OOP01.cpp
+++ A2_35_OOP01.cpp
@@ -1,138 +1,81 @@
-/*
-NAME - Durvesh Ravindra Kshirsagar
-CLASS - csd SE
-ROLL no. - 35
-subj. - OOP
-*/
-
 /*Implement a class Complex which represents the Complex Number data type. Implement the
 following
 1. Constructor (including a default constructor which creates the complex number 0+0i).
 2. Overload operator+ to add two complex numbers.
 3. Overload operator* to multiply two complex numbers.
 4. Overload operators << and >> to print and read Complex Numbers.*/
-
 #include<iostream>
 using namespace std;
-
 class complex
 {
-	public:
-		float real;
-		float imag;
-	
-	complex()
-	{
-		real=0;
-		imag=0;
-	}
-	friend ostream &operator>>(complex&in,complex&c);
-	friend ostream &operator<<(complex&out,complex&c);
-	complex operator+(complex t);
-	complex operator*(complex t);
-	
+    private:
+        float real;
+        float img;
+    public:
+    complex()
+    {
+        real=0;
+        img=0;
+    }
+    friend istream &operator>>(istream&in,complex&c);
+    friend ostream &operator<<(ostream&out,complex&c);
+        complex operator+(complex t);
+        complex operator*(complex t);
 };
 istream &operator>>(istream&in,complex&c)
 {
-      cout<<"\nEnter real and imaginary parts of number:";
-      in>>c.real;
-      in>>c.imag;
+    cout<<"\nENTER REAL AND IMAGINARY PART OF NOS.:";
+    in>>c.real>>c.img;
 }
-   
 ostream &operator<<(ostream&out,complex&c)
-{   cout<<"\nThe complex no is:";
-    out<<c.real<<"+"<<c.imag<<"i\n";
+{
+    cout<<"\nENTER REAL AND IMAGINARY PART OF NOS.:";
+    out<<c.real<<"+"<<c.img<<"i";
 }
-
 complex complex::operator+(complex t)
 {
-	complex temp;
-	temp.real=real + c2.real;
-	temp.imag=imag + c2.imag;
-	return temp;
+    complex temp;                           
+    temp.real=real + t.real;
+    temp.img=img + t.img;
+    return temp;
 }
 complex complex::operator*(complex t)
 {
-	complex temp;
-	temp.real=(real*c2.real)-(imag*c2.imag);
-	temp.imag=(real*c2.imag)+(c2.real*imag);
-	return temp;
+    complex temp;
+    temp.real=(real*t.real)-(img*t.img);
+    temp.img=(real*t.img)+(real*t.img);
+    return temp;
 }
 
 int main()
 {
-	complex c1,c2,c3;
-	
-	cout<<"\nFOR 1ST COMPLEX NO";
-    	cin>>c1;
-      	cout<<c1;
-     
-	cout<<"\nFOR 2ND COMPLEX NO";
-	cin>>c2;
-    	cout<<c2;
-	
-	int choice;
-	char ch;
-	
-	do{
-
-	cout<<"\n----------------*MENU*-------------------\n1.ADDITION \n2.MULTIPLICATION \n-----------------------------------------\n";
-	cout<<"ENTER THE OPERATION WHICH IS TO BE PERFORMED:";
-	cin>>choice;
-
-	switch(choice)	
-	{
-		case 1:	cout<<"\n PERFORMING C1 + C2";
-			c3=c1+c2;
-			cout<<c3;
-			break;
-		case 2:	cout<<"\n PERFORMING C1 * C2";
-			c3=c1*c2;
-			cout<<c3;
-			break;
-		
-		default: cout<<"INVALID CHOICE";
-	}
-	cout<<"\ndo you want to return menu(Y/N):";
-	cin>>ch;
-	}while(ch=='Y'|ch=='y');
-	
-	return 0;
+    complex c1,c2,c3;
+    
+    cout<<"\nFOR 1st COMPLEX NO:";
+    cin>>c1;
+    cout<<c1;
+    
+    cout<<"\nFOR 2nd COMPLEX NO:";
+    cin>>c2;
+    cout<<c2;
+    
+    int choice;
+do
+{   
+    cout<<"\nENTER OPERATION THAT YOU WANT TO PERFORM\n :\n1. ADDITION \n2. MULTIPLICATION\n";
+    cin>>choice;
+ 
+    switch(choice)
+    {
+        case 1: cout<<"\nPERMORM C1+C2";
+            c3=c1+c2;
+            cout<<c3;
+            break;
+        case 2: cout<<"\nPERMORM C1*C2";
+            c3=c1*c2;
+            cout<<c3;
+            break;
+        default : cout<<"INVALID CHOICE";
+    }
+} while (choice!=3);
 }
-
-
-/*Output:
-FOR 1ST COMPLEX NO
-Enter real and imaginary parts of number:33
-44
-
-The complex no is:33+44i
-
-FOR 2ND COMPLEX NO
-Enter real and imaginary parts of number:55
-66
-
-The complex no is:55+66i
-
-----------------*MENU*-------------------
-1.ADDITION 
-2.MULTIPLICATION 
------------------------------------------
-ENTER THE OPERATION WHICH IS TO BE PERFORMED:1
-
- PERFORMING C1 + C2
-The complex no is:88+110i
-
-do you want to return menu(Y/N):Y
-
-----------------*MENU*-------------------
-1.ADDITION 
-2.MULTIPLICATION 
------------------------------------------
-ENTER THE OPERATION WHICH IS TO BE PERFORMED:2
-
- PERFORMING C1 * C2
-The complex no is:-1089+4598i
-
-do you want to return menu(Y/N):n
-*/
